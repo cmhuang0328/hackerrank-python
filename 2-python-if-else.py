a7a52b5ded7c01feb4ebb15fcd4c7a01730f6570
Chien-Ming Huang <cmhuang0328@gmail.com>
2017-12-02 14:24:03 -0500

    Create 2-python-if-else.py

---
+++ 2-python-if-else.py
@@ -0,0 +1,23 @@
+# In Python 3
+“”“
+Task
+Given an integer, , perform the following conditional actions:
+If n is odd, print Weird
+If n is even and in the inclusive range of  to , print Not Weird
+If n is even and in the inclusive range of  to , print Weird
+If n is even and greater than , print Not Weird
+
+Input Format
+A single line containing a positive integer, n.
+
+Constraints
+1 <= n <= 100
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    # range function (x, y) means x to y - 1
+    if ( n in range(6, 21) or n % 2.0 == 1):
+        print("Weird")
+    else:
+        print("Not Weird")
